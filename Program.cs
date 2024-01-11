7013dc6dcdbc953dc140db9132752be07f06f443
PatrickStrogueia <patrickdealcantara@gmail.com>
2024-01-11 15:08:55 -0400

    Finalizando o projeto da calculadora

--- Program.cs
+++ Program.cs
@@ -18,10 +18,21 @@ namespace Calculator
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Divisão");
             Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
 
             Console.WriteLine("-----------------");
             Console.WriteLine("Selecione uma opção:");
             short res = short.Parse(Console.ReadLine());
+
+            switch(res) 
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
         }
 
         static void Soma()
@@ -42,6 +53,7 @@ namespace Calculator
             // Console.WriteLine($"O resultado da soma é: {v1 + v2}");
             // Console.WriteLine("O resultado da soma é: " + (v1 + v2));
             Console.ReadKey();
+            Menu();
         }
     
         static void Subtracao()
@@ -59,6 +71,7 @@ namespace Calculator
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração é {resultado}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Divisao()
@@ -75,7 +88,8 @@ namespace Calculator
 
             float resultado = v1 / v2;
             Console.WriteLine($"O resultado da divisão é {resultado}");
-            Console.ReadKey();                    
+            Console.ReadKey();
+            Menu();
         }
 
         static void Multiplicacao()
@@ -93,6 +107,7 @@ namespace Calculator
             float resultado = v1 * v2;
             Console.WriteLine("O resultado da multiplicação é " + (v1 * v2));
             Console.ReadKey();
+            Menu();
         }
 
     }
